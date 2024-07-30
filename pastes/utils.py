c8d5784cd3de766ad8ad36e81752f19e0801aef0
TanirbergenDos <tanirbergendos2006@gmail.com>
2024-07-31 00:03:37 +0500

    add is_less_than_1mb function

--- pastes/utils.py
+++ pastes/utils.py
@@ -4,5 +4,23 @@ from django.utils.baseconv import BASE64_ALPHABET
 
 
 # URL_SAFE_BASE64 = ascii_uppercase + ascii_lowercase + digits + "-_"
-def random_url():
-    return "".join(choice(BASE64_ALPHABET) for _ in range(8))
+def random_url(length=8):
+    """Return randomly generated string in Base64"""
+    return "".join(choice(BASE64_ALPHABET) for _ in range(length))
+
+
+def is_less_than_1mb(text):
+    """Return True if text is less than 1 MB (1_000_000 bytes)"""
+    if len(text.encode('utf-8')) >= 1_000_000:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    pass
+    # test_string = " \\你 （づ￣3￣）づ╭❤️～好"
+    # test_string = "你"
+    # print("The original string : " + str(test_string))
+
+    # print("The length of string in bytes :", len(test_string.encode('utf-8')))
+    # print("The length of string in bytes :", len(test_string.encode('utf-16')))
--- pastes/views.py
+++ pastes/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
 from .models import Paste
-from .utils import random_url
+from .forms import AddPasteForm
+from .utils import random_url, is_less_than_1mb
+
+
+URL_LENGTH = 6
 
 
 def create_paste(request):
     if request.method == "POST":
-        url = random_url()
-        Paste.objects.create(text=request.POST["text"], url=url)
-        return HttpResponseRedirect(reverse("pastes:show", args=(url,)))
-    return render(request, "pastes/create_paste.html")
+        url = random_url(URL_LENGTH)
+        data = request.POST.copy()
+        if is_less_than_1mb(data.get("text")):
+            if not data.get("url").strip():
+                data.update({"url": url})
+
+            form = AddPasteForm(data=data)
+            if form.is_valid():
+                form.save()
+
+                return redirect(reverse("pastes:show", args=(data["url"],)))
+
+        form = AddPasteForm(data=request.POST)
+    else:
+
+        form = AddPasteForm()
+    context = {"form": form}
+    return render(request, "pastes/create_paste.html", context)
 
 
 def show_paste(request, pk):
-    context = {
-        "paste": Paste.objects.get(url=pk)
-    }
+    context = {"paste": Paste.not_expired.get(url=pk)}
     return render(request, "pastes/show_paste.html", context)
