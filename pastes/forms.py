f07dfaa446931282fbdd9881041c28cc69c3b1fe
TanirbergenDos <tanirbergendos2006@gmail.com>
2024-09-01 13:29:08 +0500

    add validator

--- pastes/forms.py
+++ pastes/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import ValidationError
+from django.utils.baseconv import BASE64_ALPHABET
 
 from .models import Paste
 
@@ -9,11 +11,20 @@ class AddPasteForm(forms.ModelForm):
         fields = ["text", "url", "expired_at"]
 
         widgets = {
-            "url": forms.TextInput(attrs={"required": False}),
             "expired_at": forms.TextInput(attrs={"type": "datetime-local"}),
         }
 
     def __init__(self, *args, **kwargs):
-        super(AddPasteForm, self).__init__(*args, **kwargs)
-        # Set `custom_url` to not required
+        super().__init__(*args, **kwargs)
+        # Set `custom_url` and `expiration_date` to not required
         self.fields["url"].required = False
+        self.fields["expired_at"].required = False
+
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        if not (set(url) <= set(BASE64_ALPHABET)):
+            raise ValidationError(
+                "Only English characters, numbers, hyphens and underscores are allowed."
+            )
+
+        return url
